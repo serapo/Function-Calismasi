c4adbe5812ec05183e3e66ecd114f929ca736ba6
Serap <77828715+serapo@users.noreply.github.com>
2022-05-16 13:49:43 +0300

    first commit

---
+++ function.js
@@ -0,0 +1,37 @@
+// SORU 1:TabanTabanve yükseklik değerlerini parametre olarakolarakalan ve bir üçgenin alanını hesaplayarakhesaplayarakana programa döndüren fonksiyonu yazınızyazınız.
+
+// Üçgenin Alanı = (taban x yükseksik )/2
+
+// function ucgenAlan(taban,yukseklik){
+
+//     let alan=(taban*yukseklik)/2;
+//     console.log(alan);
+// }
+
+// ucgenAlan(4,5);
+
+
+// SORU 2:kareAl,küpAl,üsAl şeklinde şeklindeüç adet farklı arrow fonksiyonu tanımlayın tanımlayın.Bu fonksiyonların ana programdan gereken parametreleri alarak sonuçları sonuçları ana programa döndürmeli gerekmektedir gerekmektedir.
+
+// const kareAl=num =>(num**2);
+// const küpAl=num =>(num**3);
+// const üsAl=(taban,üs) => (taban**üs);
+
+// console.log(kareAl(4));
+// console.log(küpAl(4));
+// console.log(üsAl(3,5));
+
+
+// SORU 3:Yıl değerini parametre olarak alan ve bu yılın artık yıl olup olmadığını hesaplayarak sonucu ana programa döndüren fonksiyonu function-expression yöntemi ile yazınız.NOT:Yıl 4’e tam bölünüyorsa bölünüyorsa VE (100 'e tam bölünbölünmüyorsayorsaVEYA 400 'e tam bölünüyorsa) artık yıldır aksi takdirde değildirdeğildir.
+
+// const yıl=function(year){
+//     if(year%4==0 && (year%100!=0 || year%400==0)){
+//         return year;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+// console.log(yıl(2022));
+
